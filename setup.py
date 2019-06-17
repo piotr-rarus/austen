509a30c988ff708a4b7cf7dec4a91af1907676c8
Piotr Rarus <piotr.rarus@gmail.com>
2019-06-17 11:32:15 +0200

    updated dependencies

--- setup.py
+++ setup.py
@@ -7,13 +7,12 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='austen',
-    version='0.1.0',
-    author='Piotr Rarus',
-    author_email='piotr.rarus@gmail.com',
     description='Nested telemetry logger.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/piotr-rarus/austen',
+    license='MIT',
+    version='0.1.0',
     packages=setuptools.find_packages(
         exclude=[
             "tests",
@@ -26,11 +25,15 @@ setuptools.setup(
         'matplotlib',
         'seaborn',
         'scikit-image',
-        'joblib'
+        'joblib',
+        'pytest',
+        'pytest-cov'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: MIT',
         'Operating System :: OS Independent'
     ],
+    author='Piotr Rarus',
+    author_email='piotr.rarus@gmail.com',
 )
